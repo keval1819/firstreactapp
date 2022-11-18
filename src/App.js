2130af3285c629d4250f1a7e65d1f59531670a00
keval1819 <kevalshiyani1819@gmail.com>
2022-11-18 15:28:28 +0530

    second day change

--- src/App.js
+++ src/App.js
@@ -18,9 +18,11 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <h1>keval shiyani</h1>
+      {/* <h1>keval shiyani</h1>
       <h3>course : web devloper</h3>
-      <p>Copyright OpenJS Foundation and Node.js contributors. All rights reserved. The OpenJS Foundation has registered trademarks and uses trademarks. For a list of trademarks of the OpenJS Foundation, please see our Trademark Policy and Trademark List. Trademarks and logos not indicated on the list of OpenJS Foundation trademarks are trademarks™ or registered® trademarks of their respective holders. Use of them does not imply any affiliation with or endorsement by them.</p>
+      <p>Copyright OpenJS Foundation and Node.js contributors. All rights reserved. The OpenJS Foundation has registered trademarks and uses trademarks. For a list of trademarks of the OpenJS Foundation, please see our Trademark Policy and Trademark List. Trademarks and logos not indicated on the list of OpenJS Foundation trademarks are trademarks™ or registered® trademarks of their respective holders. Use of them does not imply any affiliation with or endorsement by them.</p> */}
+
+      <h1>Hello world</h1>
     </div>
   );
 }
